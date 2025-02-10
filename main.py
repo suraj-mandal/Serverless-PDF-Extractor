5c1a95b4f15ff636ca1aa4e047067200f4455a89
suraj-mandal <suraj.mandal738@gmail.com>
2025-02-10 23:49:38 +0530

    Setup automatic tests

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-def is_working_correctly():
-    return True
-
-
 if __name__ == '__main__':
-    if is_working_correctly():
-        print("Code is working correctly")
-    else:
-        print("Code is NOT working correctly")
+    print('Hello World')
--- tests/test.py
+++
@@ -1,5 +0,0 @@
-from unittest import TestCase
-
-class Test(TestCase):
-    def test_addition(self):
-        self.assertEqual(1 + 1, 2)
