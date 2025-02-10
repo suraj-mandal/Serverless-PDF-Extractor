3376184df3630f7a1e1380dc1e4e1b90656e1cba
suraj-mandal <suraj.mandal738@gmail.com>
2025-02-11 01:02:17 +0530

    Finished development of pdf extraction service

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+import asyncio
+
+from services.pdf_service import PdfService
+
+
+async def async_main():
+    pdf_service = PdfService('https://www.antennahouse.com/hubfs/xsl-fo-sample/pdf/basic-link-1.pdf')
+    await pdf_service.extract_content()
+    res = pdf_service.extract_pages_from_pdf()
+    print(len(res))
+    # print(res)
+
+
 if __name__ == '__main__':
-    print('Hello World')
+    asyncio.run(async_main())
