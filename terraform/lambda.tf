63f245cf75fc8c676eab5eaf0b4dac103b541e56
suraj-mandal <suraj.mandal738@gmail.com>
2025-02-12 00:34:33 +0530

    Updated lamdbda.tf

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,9 +1,25 @@
+resource "null_resource" "install_dependencies" {
+  provisioner "local-exec" {
+    command = <<EOT
+      rm -rf ../package && mkdir ../package
+      pip install -r ../requirements.txt -t ../package
+      cp -r ../lambda/* ../package/
+      cd ../package && zip -r ../lambda_function.zip .
+    EOT
+  }
+
+  triggers = {
+    always_run = timestamp()
+  }
+}
+
 resource "aws_lambda_function" "pdf_extractor_lambda" {
   function_name    = var.lambda_function_name
   role             = aws_iam_role.lambda_role.arn
   handler          = "main.lambda_handler"
   runtime          = "python3.10"
-  filename         = "lambda_function.zip"
+  filename         = "../lambda_function.zip"
   timeout          = 10
 
+  depends_on = [null_resource.install_dependencies]  # 👈 Ensure ZIP is created first
 }
