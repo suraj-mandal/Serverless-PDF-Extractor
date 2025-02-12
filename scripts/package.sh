9c94e89b15a363705d591e84c89427c54f81748a
suraj-mandal <suraj.mandal738@gmail.com>
2025-02-12 19:49:29 +0530

    Fixed packaging script

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 rm -rf ../package && mkdir ../package
-pip install -r ../requirements.txt -t ../package
+pip install -r requirements.txt -t ../package
 cp -r ../lambda/* ../package/
