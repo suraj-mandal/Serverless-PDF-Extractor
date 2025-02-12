9848cd97b5cf708d18b57a1c3048b469e478f807
suraj-mandal <suraj.mandal738@gmail.com>
2025-02-13 00:36:30 +0530

    Fixed memory issue

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,29 +1,18 @@
-# resource "null_resource" "install_dependencies" {
-#   provisioner "local-exec" {
-#     command = <<EOT
-#       sh ../scripts/package.sh
-#     EOT
-#   }
-#
-#   triggers = {
-#     always_run = timestamp()
-#   }
-# }
-
-data archive_file "lambda" {
+data "archive_file" "lambda" {
   type        = "zip"
   source_dir  = "${path.module}/package"
   output_path = "${path.module}/lambda_function.zip"
 }
 
 resource "aws_lambda_function" "pdf_extractor_lambda" {
-  function_name = var.lambda_function_name
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "main.lambda_handler"
-  runtime       = "python3.10"
-  filename      = "lambda_function.zip"
-  layers        = [aws_lambda_layer_version.poppler_layer.arn]
-  timeout       = 300
+  function_name    = var.lambda_function_name
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "main.lambda_handler"
+  runtime          = "python3.10"
+  filename         = "lambda_function.zip"
+  layers           = [aws_lambda_layer_version.poppler_layer.arn]
+  timeout          = 300
+  memory_size      = 1024
   source_code_hash = data.archive_file.lambda.output_base64sha256
   # depends_on    = [null_resource.install_dependencies]  # 👈 Ensure ZIP is created first
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 resource "aws_lambda_layer_version" "poppler_layer" {
-  layer_name = "poppler_layer"
-  filename   = "${path.module}/data/poppler.zip"
+  layer_name               = "poppler_layer"
+  filename                 = "${path.module}/data/poppler.zip"
   compatible_architectures = ["x86_64", "arm64"]
-  compatible_runtimes = ["python3.10"]
+  compatible_runtimes      = ["python3.10"]
 }
